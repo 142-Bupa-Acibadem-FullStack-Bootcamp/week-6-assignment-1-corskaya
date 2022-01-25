6a9727569c855aaee14bb738029df5bb8ffba269
Çağrı Örskaya <cagriorskaya@hotmail.com>
2022-01-25 16:06:09 +0300

    test cases added

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ function twoSum(nums, target) {
     }
     return [];
 };
+
+/////////// Test Cases ///////////
+
+console.log(twoSum([2, 7, 11, 15], 9)); // should return [0, 1]
+
+console.log(twoSum([3, 2, 4], 6)); // should return [1, 2]
+
+console.log(twoSum([3, 3], 6)); // should return [0, 1]
